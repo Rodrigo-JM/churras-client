de4ec2772bb8def13c45179a5ca43e63fcb67585
Rodrigo Jappur Mallmann <rodrigo.mallmann@saipos.com>
2021-11-15 23:01:13 -0300

    ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'churras-client',
+      script: 'yarn',
+      args: 'run start',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
